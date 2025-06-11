57a191f15f7ef3bec078743ffb8ba09d6cfd29aa
khalid <109499271+Khalidmuhammad25@users.noreply.github.com>
2025-06-11 13:29:05 +0600

    git init added

---
+++ init.py
@@ -0,0 +1,20 @@
+import os
+
+def vcs_init(path='.'):
+    vcs_dir = os.path.join(path, '.vcs')
+
+    if os.path.exists(vcs_dir):
+        print("Repository already initialized.")
+        return
+
+    os.makedirs(os.path.join(vcs_dir, 'objects'))    
+    os.makedirs(os.path.join(vcs_dir, 'refs'))        
+    with open(os.path.join(vcs_dir, 'HEAD'), 'w') as f:
+        f.write('ref: refs/heads/main\n')           
+    with open(os.path.join(vcs_dir, 'config'), 'w') as f:
+        f.write('[core]\n\trepositoryformatversion = 0\n')
+
+    print(f"Initialized empty VCS repository in {os.path.abspath(vcs_dir)}")
+
+
+vcs_init()
